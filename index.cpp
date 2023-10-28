fe2147a296ee663a7e8dfc02aa1e2344204a9c13
Aditi <aditisharma3711.as@gmail.com>
2023-10-28 16:11:07 +0530

    index.cpp modified

--- index.cpp
+++ index.cpp
@@ -5,7 +5,7 @@ int main(){
     int a , b;
     cout<<"enter a ";
     cin>> a;
-    cout<<"enter b ";
+    cout<<"enter b (an integer value) ";
     cin>> b;
     cout<<"sum of "<< a<<" & "<<b<<" is "<< a+b ;
 }
